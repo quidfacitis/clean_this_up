6f585a1b1a391f03e5a9ca59b466275aa989757c
Ross Loney <majoross@gmail.com>
2021-04-20 22:04:16 -0500

    Add edit, delete, assign task, and task count to StaffMember

--- client/components/StaffMember.jsx
+++ client/components/StaffMember.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Icon, InlineIcon } from '@iconify/react';
+import delete24Filled from '@iconify-icons/fluent/delete-24-filled';
+import editSolid from '@iconify/icons-clarity/edit-solid';
 
 const StaffMember = ({member}) => {
   return (
     <div className="staff-member-card">
-      <div>{member.name}</div>
-      <div>{member.role}</div>
+      <div className="staff-name">{member.name}</div>
+      <div className="staff-role">{member.role}</div>
+      <div className="staff-card-assign-task">Assign Task</div>
+      <div className="staff-edit-delete-container">
+        <span className="staff-edit"><Icon icon={editSolid} /></span>
+        <span className="staff-delete"><Icon icon={delete24Filled} /></span>
+      </div>
+      <div className="staff-card-task-count">3</div>
     </div>
   );
 };
