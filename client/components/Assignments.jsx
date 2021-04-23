88ae179db6882441f3f96d50ba614ebc398e6ac0
Ross Loney <majoross@gmail.com>
2021-04-23 11:18:58 -0500

    Incorporate toggle switch for urgent assignments

--- client/components/Assignments.jsx
+++ client/components/Assignments.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
-import { Icon } from '@iconify/react';
+import React, {Component} from 'react';
+import {Icon, InlineIcon} from '@iconify/react';
 import plusSquareFill from '@iconify/icons-bi/plus-square-fill';
+import alertUrgent16Filled from '@iconify-icons/fluent/alert-urgent-16-filled';
 import AssignmentItem from './AssignmentItem.jsx';
+import Switch from 'react-switch';
 
-const Assignments = ({assignments, toggleAssignmentForm, toggleMessageModal}) => {
-  const assignmentItems = assignments.map(a => <AssignmentItem assignment={a} toggleMessageModal={toggleMessageModal} />);
+class Assignments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false
+    }
+    this.toggleSwitch = this.toggleSwitch.bind(this);
+  }
 
-  return (
-    <div className="staff-container">
-      <div className="staff-navbar">
-        <h2 className="staff-title">Assignments</h2>
+  toggleSwitch() {
+    const {checked} = this.state;
+    this.setState({
+      checked: !checked
+    });
+  }
+
+  render() {
+    let {assignments, toggleAssignmentForm, toggleMessageModal} = this.props;
+    const {checked} = this.state;
+
+    if (checked) {
+      assignments = assignments.filter((a) => a.urgent === true);
+    }
+
+    const assignmentItems = assignments.map(a => <AssignmentItem assignment={a} toggleMessageModal={toggleMessageModal} />);
+
+    return (
+      <div className="staff-container">
+        <div className="assignment-navbar">
+          <h2 className="staff-title">Assignments</h2>
+          <label className="urgent-switch-container">
+            <div className="urgent-switch-msg">Only<span className="urgent-switch-icon"><InlineIcon icon={alertUrgent16Filled} width="1.5em" color="#737078" /></span></div>
+            <Switch onChange={this.toggleSwitch} checked={checked} onColor="#4cac84" offColor="#737078"/>
+          </label>
+        </div>
+        <div className="staff-member-container">
+          <span className="staff-new-member-btn" onClick={toggleAssignmentForm}><Icon icon={plusSquareFill} color="#4cac84" width="4em"/></span>
+          {assignmentItems}
+        </div>
       </div>
-      <div className="staff-member-container">
-        <span className="staff-new-member-btn" onClick={toggleAssignmentForm}><Icon icon={plusSquareFill} color="#4cac84" width="4em"/></span>
-        {assignmentItems}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Assignments;
